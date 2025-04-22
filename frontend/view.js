8438ae9d1fd7a6b3271bd15889c7aaad0f178657
usmanrz <usman.m.razzaq@gmail.com>
2025-04-22 13:57:02 -0700

    add reactjs config

---
+++ frontend/view.js
@@ -0,0 +1,13 @@
+// View.js
+import React from 'react';
+
+function View() {
+  return (
+    <div>
+      <h1>Welcome to My View</h1>
+      <p>This is a simple React component.</p>
+    </div>
+  );
+}
+
+export default View;
